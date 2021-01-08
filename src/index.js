2f790dbd6b02cd9472ea1bd5eac9b99d75053141
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-01-08 15:29:29 +0100

    button event listeners added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("test")
+
+
+// Define elements
+const closeModal = document.querySelector("#close-modal");
+const modalSubmitButton = document.querySelector("#modal-submit-button");
+const projectSubmitButton = document.querySelector("#project-submit-button");
+const addTaskButton = document.querySelector("#add-task-button");
+const modalDiv = document.getElementById("modal");
+
+
+// add Event Listeners
+closeModal.addEventListener("click", (e) => {
+    modalDiv.style.display = "none"
+});
+
+modalSubmitButton.addEventListener("click", (e) => {
+    modalDiv.style.display = "none"
+});
+
+projectSubmitButton.addEventListener("click", (e) => {
+    console.log(e.target)
+});
+
+addTaskButton.addEventListener("click", (e) => {
+    modalDiv.style.display = "block"
+});
